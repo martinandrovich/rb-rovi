baa59f6df8fc1e1f930e766d25f6f3894f5a7cf0
Slaxzer <peter@qrholm.dk>
2020-10-26 21:39:08 +0100

    .

--- ws/src/rovi_pose_estimator/include/rovi_pose_estimator/rovi_pose_estimator.h
+++ ws/src/rovi_pose_estimator/include/rovi_pose_estimator/rovi_pose_estimator.h
@@ -1,8 +1,24 @@
 #pragma once
 #include <string>
-
+#include <opencv2/opencv.hpp>
 namespace rovi_pose_estimator
 {
+    namespace M2
+    {
+        using namespace rovi_pose_estimator::M2;
+
+        void
+        get_depth_image();
+
+
+
+
+    }
     void
 	test(const std::string& str);
+
+
+
+   
+    //void rovi_pose_estimator::
 }
--- ws/src/rovi_pose_estimator/src/pose_example.cpp
+++ ws/src/rovi_pose_estimator/src/pose_example.cpp
@@ -11,7 +11,6 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg, const std::string& im_
   try
   {
     cv::imshow(im_window, cv_bridge::toCvShare(msg, "bgr8")->image );
-	cv::waitKey(1);
   }
   
   catch (cv_bridge::Exception& e)
@@ -33,21 +32,24 @@ main(int argc, char** argv)
 
 	// log information
 	ROS_INFO("Initialized a single-thread ROS example node.");
-	cv::namedWindow("stereo_left");
-	cv::namedWindow("stereo_right");
-	cv::startWindowThread();
+	//cv::namedWindow("stereo_left");
+	//cv::namedWindow("stereo_right");
+	//cv::startWindowThread();
 
-	image_transport::ImageTransport it(nh);
-	//image_transport::Subscriber sub = it.subscribe("rbrovi/camera_stereo/left/image_raw", 1, &imageCallback, image_transport::TransportHints("left_image"));
-	image_transport::Subscriber sub2 = it.subscribe("rbrovi/camera_stereo/right/image_raw", 1, &imageCallback);
-	ros::spin();
-	cv::destroyAllWindows();
+	// image_transport::ImageTransport it(nh);
+	// //image_transport::Subscriber sub = it.subscribe("rbrovi/camera_stereo/left/image_raw", 1, &imageCallback, image_transport::TransportHints("left_image"));
+	// image_transport::Subscriber sub2 = it.subscribe("rbrovi/camera_stereo/right/image_raw", 1, &imageCallback);
+	// ros::spin();
+	// cv::destroyAllWindows();
 
 	// const auto& sub = nh.subscribe("rbrovi/camera_stereo/left/image_raw", 1, &imageCallback);
 	// use rovi_gazebo library
 	// rovi_pose_estimator::test("test");
 	// cv::Mat img;
 
+	cv::namedWindow("stereo_right");
+	cv::startWindowThread();
+	ros::Rate loop_rate(1000);
 	while (ros::ok())
 	{
 		const auto msg = ros::topic::waitForMessage<sensor_msgs::Image>("/rbrovi/camera_stereo/left/image_raw");
@@ -57,9 +59,10 @@ main(int argc, char** argv)
 		{
 			ROS_INFO("Got new image!");
 			const auto img = cv_bridge::toCvShare(msg, "bgr8")->image;
-			cv::imshow("img", img);
-			cv::waitKey(0);
+			cv::imshow("stereo_right", img);
+			cv::waitKey(1);
 		}
+		loop_rate.sleep();
 
 	}
 	
