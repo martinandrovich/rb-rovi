7ede8516454e97525f79da095505eeb8f077d2ae
Slaxzer <peter@qrholm.dk>
2020-10-26 11:17:37 +0100

    suscribing to imgages using image_transport

--- ws/src/rovi_pose_estimator/src/pose_example.cpp
+++ ws/src/rovi_pose_estimator/src/pose_example.cpp
@@ -4,13 +4,14 @@
 #include <opencv2/opencv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <ros/topic.h>
+#include <image_transport/image_transport.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+void imageCallback(const sensor_msgs::ImageConstPtr& msg, const std::string& im_window)
 {
   try
   {
-    cv::imshow("stereo_left", cv_bridge::toCvShare(msg, "bgr8")->image );
-	cv::waitKey(30);
+    cv::imshow(im_window, cv_bridge::toCvShare(msg, "bgr8")->image );
+	cv::waitKey(1);
   }
   
   catch (cv_bridge::Exception& e)
@@ -27,23 +28,32 @@ main(int argc, char** argv)
 
 	ros::init(argc, argv, "example_node");
 	ros::NodeHandle nh;
-	cv::namedWindow("stereo_left");
+	
 
 	// log information
 	ROS_INFO("Initialized a single-thread ROS example node.");
+	cv::namedWindow("stereo_left");
+	cv::namedWindow("stereo_right");
+	cv::startWindowThread();
+
+	image_transport::ImageTransport it(nh);
+	//image_transport::Subscriber sub = it.subscribe("rbrovi/camera_stereo/left/image_raw", 1, &imageCallback, image_transport::TransportHints("left_image"));
+	image_transport::Subscriber sub2 = it.subscribe("rbrovi/camera_stereo/right/image_raw", 1, &imageCallback);
+	ros::spin();
+	cv::destroyAllWindows();
 
 	// const auto& sub = nh.subscribe("rbrovi/camera_stereo/left/image_raw", 1, &imageCallback);
 	// use rovi_gazebo library
 	// rovi_pose_estimator::test("test");
 
-	while(ros::ok())
-	{
-		auto msg = ros::topic::waitForMessage<sensor_msgs::Image>( "rbrovi/camera_stereo/left/image_raw", nh);
-		//imageCallback(msg);
-		cv::imshow("stereo_left2", cv_bridge::toCvShare(msg, "bgr8")->image );
-		cv::waitKey(0);
-	
-	}
+	//while(ros::ok())
+	//{
+	//	auto msg = ros::topic::waitForMessage<sensor_msgs::Image>( "rbrovi/camera_stereo/left/image_raw", nh);
+	//	//imageCallback(msg);
+	//	cv::imshow("stereo_left2", cv_bridge::toCvShare(msg, "bgr8")->image );
+	//	cv::waitKey(0);
+//
+	//}
 	
 
 	//nh.subscribe("/rbrovi/camera_stereo/left/image_raw");
