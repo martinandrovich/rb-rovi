e0e72f36cd14de52b6d388be77cf7f06a69eddf3
VeryPanda <dscho15@student.sdu.dk>
2020-11-17 16:14:35 +0100

    all

--- ws/src/ur5_moveit_config/src/rrt_test.cpp
+++ ws/src/ur5_moveit_config/src/rrt_test.cpp
@@ -14,12 +14,15 @@
 #include <rovi_utils/rovi_utils.h>
 
 // arm and robot_description
-constexpr auto ARM_GROUP    	 = "ur5_arm";
-constexpr auto WSG_GROUP 	     = "wsg";
-constexpr auto ROBOT_DESCRIPTION = "robot_description";
-constexpr auto PLANNER_PLUGIN_NAME = "ompl_interface/OMPLPlanner";
+constexpr auto ARM_GROUP    	 	= "ur5_arm";
+constexpr auto WSG_GROUP 	    	= "wsg";
+constexpr auto ROBOT_DESCRIPTION 	= "robot_description";
+constexpr auto PLANNER_PLUGIN_NAME 	= "ompl_interface/OMPLPlanner";
 
 //https://github.com/ros-planning/moveit_tutorials/blob/master/doc/creating_moveit_plugins/lerp_motion_planner/src/lerp_example.cpp
+
+void 
+probabalistic_planner();
 
 int
 main(int argc, char** argv)
@@ -30,25 +33,19 @@ main(int argc, char** argv)
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
 
-	// we we will use movegroupinterface to interface the planning group
-	// moveit::planning_interface::MoveGroupInterface move_group(ARM_GROUP);
-
-	// we will use planning-scene interface to add and remove collision objects in our virtual world
-	//moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
 	// load robot model and kinematic model, and use it to setup the planning scene
     robot_model_loader::RobotModelLoaderPtr robot_model_loader(new robot_model_loader::RobotModelLoader(ROBOT_DESCRIPTION));
-    robot_model::RobotModelPtr robot_kinematic_model(robot_model_loader->getModel());
+    robot_model::RobotModelPtr robot_model(robot_model_loader->getModel());
 
     // set up the planning scene for collision detection
-    planning_scene::PlanningScene planning_scene(robot_kinematic_model);
+    planning_scene::PlanningScenePtr planning_scene( new planning_scene::PlanningScene(robot_model) );
 
 	// visualization in rviz
 	moveit_visual_tools::MoveItVisualTools visual_tools("ur5_link0");
 	visual_tools.deleteAllMarkers();
 	
     // get robot state, this is a raw reference and pointers for arm_group and wsg_group
-    auto& robot_state    = planning_scene.getCurrentStateNonConst();
+    auto& robot_state    = planning_scene->getCurrentStateNonConst();
     const auto arm_group = robot_state.getJointModelGroup(ARM_GROUP);
     const auto wsg_group = robot_state.getJointModelGroup(WSG_GROUP);
 
@@ -60,78 +57,131 @@ main(int argc, char** argv)
 	moveit_msgs::PlanningScene planning_scene_msg;
 
 	// move the base of the robot
-	rovi_utils::move_base(robot_state, {0.1, 0.1, 0.75});
+	// rovi_utils::move_base(robot_state, {0.1, 0.1, 0.75});
 
 	// insert table and other constant objects into the planning scene
 	std::vector<moveit_msgs::CollisionObject> collision_objects
 	{
-		rovi_utils::make_mesh_cobj("table",  planning_scene.getPlanningFrame() , {0.4, 0.6, 0.64}),
-		rovi_utils::make_mesh_cobj("bottle", planning_scene.getPlanningFrame() , {0.5, 1.0, 0.75})
+		rovi_utils::make_mesh_cobj("table",  planning_scene->getPlanningFrame() , {0.4, 0.6, 0.64}),
+		rovi_utils::make_mesh_cobj("bottle", planning_scene->getPlanningFrame() , {0.5, 1.0, 0.75})
 	};
 
+	// set default state
 	robot_state.setToDefaultValues(arm_group, "home");
 	robot_state.setToDefaultValues(wsg_group, "home");
 
+
+	// -------------------------------------------------------------------------------------------------------- //
+
+
 	boost::scoped_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager>> planner_plugin_loader;
+
 	planning_interface::PlannerManagerPtr planner_instance;
 
-	std::string planner_plugin_namexD;
-
-	nh.getParam("planning_plugin", planner_plugin_namexD);
-	
-	ROS_INFO_STREAM(planner_plugin_namexD);
-
-	ROS_INFO("lort");
-
-	try
-	{
-		planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>("moveit_core", "planning_interface::PlannerManager"));
-	}
-	catch (pluginlib::PluginlibException& ex)
-	{
-		ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
-	}
-
-	ROS_INFO("lort");
-	/*
-
-	ROS_INFO("lort");
+	planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>("moveit_core", "planning_interface::PlannerManager"));
 
 	planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(PLANNER_PLUGIN_NAME));
 
-	ROS_INFO("lort");
+	if (!planner_instance->initialize(robot_model, nh.getNamespace()))
+	{
+    	ROS_FATAL_STREAM("Could not initialize planner instance");
+		return -1;
+	}
+	
+	// Pose Goal
 
-	*/
+	ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
 
+	//visual_tools.publishRobotState(robot_state, rviz_visual_tools::GREEN);
+	visual_tools.trigger();
+	planning_interface::MotionPlanRequest req;
+	planning_interface::MotionPlanResponse res;
 
-	/*
+	// desired pose
+	geometry_msgs::PoseStamped pose;
+
+	// w.r.t this frame - default value
+	pose.header.frame_id = "ur5_link0";
+
+	// end-effector
+	pose.pose.position.x = 0.3;
+	pose.pose.position.y = 0.4;
+	pose.pose.position.z = 0.75;
+	pose.pose.orientation.w = 1.0;
+
+	// specify the tolerances
+	std::vector<double> tol_pos(3, 0.005);
+	std::vector<double> tol_ori(3, 0.005);
+
+	// kinematic_constraint
+	moveit_msgs::Constraints pose_goal = kinematic_constraints::constructGoalConstraints("ee_tcp", pose, tol_pos, tol_ori);
+
+	// specify plan group and pushback the goal_constraints?
+	req.group_name = ARM_GROUP;
+	req.goal_constraints.push_back(pose_goal);
+
+	planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
+
+	context->solve(res);
+
+	if (res.error_code_.val != res.error_code_.SUCCESS)
+	{
+		ROS_ERROR("Could not compute plan successfully");
+		return 0;
+	}
+	
+	// visualize the result
+	ros::Publisher display_publisher = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true);
+	moveit_msgs::DisplayTrajectory display_trajectory;
+
+	// visualize the trajectory
+	moveit_msgs::MotionPlanResponse response;
+	res.getMessage(response);
+
+	display_trajectory.trajectory_start = response.trajectory_start;
+	display_trajectory.trajectory.push_back(response.trajectory);
+
+	visual_tools.publishTrajectoryLine(display_trajectory.trajectory.back(), arm_group);
+	
+	visual_tools.trigger();
+
+	display_publisher.publish(display_trajectory);
+
+	/* Set the state in the planning scene to the final state of the last plan */
+	robot_state.setJointGroupPositions(arm_group, response.trajectory.joint_trajectory.points.back().positions);
+	planning_scene->setCurrentState(robot_state);
+
+	//visual_tools.publishRobotState(robot_state, rviz_visual_tools::GREEN);
+	visual_tools.trigger();
+
+	moveit::core::RobotStatePtr rbt_ptr(new moveit::core::RobotState(robot_state));
 
 	{
-		// set a desired pose for the end-effector
-		geometry_msgs::Pose target_pose;
-		target_pose.orientation.w = 1.0;
-		target_pose.orientation.x = 0.0;
-		target_pose.orientation.y = 0.0;
-		target_pose.orientation.z = 0.0;
-		target_pose.position.x = 0.3;
-		target_pose.position.y = 0.3;
-		target_pose.position.z = 0.5;
+		ros::Rate lp(1);
+		while (ros::ok())
+		{
+			ROS_INFO_STREAM( "The plan: " << res.planning_time_);
 
-		// set a targetpose
-		move_group.setPoseTarget(target_pose);
+			ROS_INFO("Lort");
 
-		// this is just planning, not moving the robot
-		moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+			auto traj = res.trajectory_->getStateAtDurationFromStart(0.1f, rbt_ptr);
 
-		bool success = (move_group.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+			ROS_INFO("Lort2");
 
-		ROS_INFO_STREAM("The plan was " << success ? "successfull" : "not successfull" );
+			if(traj)
+			{
+				rbt_ptr->printStatePositions();
+			}
 
-		visual_tools.publishTrajectoryLine(my_plan.trajectory_, joint_model_group);
-		visual_tools.trigger();
+			lp.sleep();
+		}
 	}
 
-	*/
-
 	return 0;
+}
+
+void 
+probabalistic_planner()
+{
+
 }
