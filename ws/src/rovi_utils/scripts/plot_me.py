5c8626cab1cb129b5f2d073ff1effa58d4bdf07d
Martin <martinandrovich@gmail.com>
2020-11-28 10:46:24 +0100

    move to proper script location

--- ws/src/rovi_utils/scripts/plot_me.py
+++ ws/src/rovi_utils/scripts/plot_me.py
@@ -1,22 +1,32 @@
-#!/usr/bin/env python3
-
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import sys
 
 def get_nerded(file_name):
-	array = np.loadtxt(open(file_name, "rb"), delimiter=",", skiprows=0)
-	array = array.reshape((array.shape[0], 4, 4))
-	return array
+    array = np.loadtxt(open(file_name, "rb"), delimiter=",", skiprows=0)
+    array = array.reshape((array.shape[0], 4, 4))
+    return array
 
-arr_traj_lin = get_nerded("traj_par.csv")
-arr_traj_rrt = get_nerded("traj_rrt.csv")
+if __name__ == "__main__":
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    print(f"Arguments count: {len(sys.argv)}")
 
-ax.scatter(arr_traj_rrt[:,0,3], arr_traj_rrt[:,1,3], arr_traj_rrt[:,2,3])
-ax.scatter(arr_traj_lin[:,0,3], arr_traj_lin[:,1,3], arr_traj_lin[:,2,3])
+    if len(sys.argv) != 3:
+        exit
 
-plt.show()
+    for i, arg in enumerate(sys.argv):
+        print(f"Argument: ", arg)
+    
+    # plot 1 and 2
+    array1 = get_nerded(sys.argv[1])
+    array2 = get_nerded(sys.argv[2])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(array1[:,0,3], array1[:,1,3], array1[:,2,3])
+    ax.scatter(array2[:,0,3], array2[:,1,3], array2[:,2,3])
+
+    plt.show()
