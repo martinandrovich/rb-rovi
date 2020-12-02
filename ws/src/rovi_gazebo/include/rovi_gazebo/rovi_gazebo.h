e464e2bbfb8158bcb9a900051ef0b6a997c0a31f
Martin <martinandrovich@gmail.com>
2020-12-02 14:07:16 +0100

    clean + add get_current_robot_state()

--- ws/src/rovi_gazebo/include/rovi_gazebo/rovi_gazebo.h
+++ ws/src/rovi_gazebo/include/rovi_gazebo/rovi_gazebo.h
@@ -1,9 +1,37 @@
 #pragma once
 
-#include <string>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Pose.h>
+#include <moveit_msgs/CollisionObject.h>
 
 namespace rovi_gazebo
 {
-	void
-	test(const std::string& str);
+
+	// should be moved to rovi_gazebo?
+	// add in_base_frame = true bool
+	
+	sensor_msgs::JointState
+	get_current_robot_state();
+	
+	sensor_msgs::JointState
+	get_current_gripper_state();
+	
+	geometry_msgs::Pose
+	get_current_link6_pose();
+
+	geometry_msgs::Pose
+	get_current_ee_pose();
+
+	geometry_msgs::Pose
+	get_current_tcp_pose();
+
+	geometry_msgs::Pose
+	get_link6_given_ee(const geometry_msgs::Pose& pose_ee);
+
+	geometry_msgs::Pose
+	get_link6_given_tcp(const geometry_msgs::Pose& pose_tcp);
+
+	std::vector<moveit_msgs::CollisionObject>
+	get_collision_objects();	
+	
 }
--- ws/src/rovi_gazebo/src/lib_rovi_gazebo.cpp
+++ ws/src/rovi_gazebo/src/lib_rovi_gazebo.cpp
@@ -1,10 +1,27 @@
 #include "rovi_gazebo/rovi_gazebo.h"
 
-#include <iostream>
-#include <ros/ros.h>
+#include <rovi_utils/rovi_utils.h>
 
-void
-rovi_gazebo::test(const std::string& str)
+#include <ros/ros.h>
+#include <iostream>
+#include <thread>
+
+sensor_msgs::JointState
+rovi_gazebo::get_current_robot_state()
 {
-	ROS_INFO_STREAM("hello: " << str);
+	// create and detach a thread that atomically updates the static variable in here;
+	// atomically return the variable when this method is called
+
+	static std::mutex mtx_joint_states;
+	static sensor_msgs::JointState joint_states;
+	static auto t = rovi_utils::create_async_listener("/joint_states", joint_states, mtx_joint_states);
+
+	// return mutexed value
+	while (true)
+	{
+		std::lock_guard<std::mutex> lock(mtx_joint_states);
+		if (not joint_states.name.empty())
+			return joint_states;
+		ros::Duration(0.1).sleep();
+	}
 }
