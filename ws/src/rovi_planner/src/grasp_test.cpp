c355b017858fbef7b21e8eb55711f94a5f288acd
Martin <martinandrovich@gmail.com>
2020-12-02 12:29:57 +0100

    refactor moveit_planner class + add test

--- ws/src/rovi_planner/src/grasp_test.cpp
+++ ws/src/rovi_planner/src/grasp_test.cpp
@@ -22,73 +22,127 @@
 #include <rovi_utils/rovi_utils.h>
 #include <ur5_controllers/interface.h>
 
-constexpr auto ARM_GROUP    	 	= "ur5_arm";
-constexpr auto WSG_GROUP 	    	= "wsg";
-constexpr auto ROBOT_DESCRIPTION 	= "robot_description";
-constexpr auto PLANNER_PLUGIN_NAME 	= "ompl_interface/OMPLPlanner";
+constexpr auto ARM_GROUP            = "ur5_arm";
+constexpr auto WSG_GROUP            = "wsg";
+constexpr auto ROBOT_DESCRIPTION    = "robot_description";
+constexpr auto PLANNER_PLUGIN_NAME  = "ompl_interface/OMPLPlanner";
 constexpr auto PLANNING_SCENE_TOPIC = "/planning_scene_gazebo";
 constexpr auto TRAJECTORY_TOPIC     = "/move_group/display_planned_path";
-constexpr auto JOINT_POS_TOPIC 		= "/ur5_joint_position_controller/command";
-constexpr auto WSG_TORQUE_TOPIC 	= "/wsg_hybrid_controller/command";
+constexpr auto FREQ                 = 1000.0; // Hz
 
-const std::vector<double>   HOME_ARM{0, -1.57, 1.57, 1.57, 1.57, 0};
-const std::vector<double>   HOME_WSG{0.05, 0.05};
-const std::array<double, 3> TABLE_POS{0.4, 0.6, 0.64};
-const std::array<double, 3> BOTTLE_POS{0.6, 0.999023, 0.75};
-const std::array<double, 3> BASE_OFFSET{0.1, 0.5, 0.75};
+constexpr auto HOME_ARM             = std::array{ 0., -1.57, 1.57, 1.57, 1.57, 0. };
+constexpr auto HOME_WSG             = std::array{ 0.05, 0.05 };
+constexpr auto BASE_OFFSET          = std::array{ 0.1, 0.5, 0.75 };
+constexpr auto POS_TABLE            = std::array{ 0.4, 0.6, 0.64 };
+constexpr auto POSE_BOTTLE_PICK     = std::array{ 0.50,  0.53, 0.1, 0.0, 0.0, 0.0 };
+constexpr auto POSE_BOTTLE_PLACE    = std::array{ 0.50,  0.53, 0.4, 0.0, 0.0, 0.0 };
+// constexpr auto POSE_BOTTLE_PLACE    = std::array{ 0.52, -0.35, 0.1, 0.0, 0.0, 0.0 };
 
 int
 main(int argc, char** argv)
 {
 	using namespace rovi_utils;
 
-	ros::init(argc, argv, "traj_test");
+	// init node
+	ros::init(argc, argv, "grasp_test");
 	ros::NodeHandle nh;
-	ros::AsyncSpinner spin(2);
-	spin.start();
-
-	// publisher_scene
-	ros::Publisher joint_state_pub	    = nh.advertise<sensor_msgs::JointState>(JOINT_POS_TOPIC, 1);
-	ros::Publisher wsg_state_pub 		= nh.advertise<std_msgs::Float64>(WSG_TORQUE_TOPIC, 1);
 
 	// init moveit_object
+	ROS_INFO_STREAM("Initializing moveit planner and scene...");
 	rovi_planner::moveit_planner::init(nh);
-
-	ur5_controllers::wsg::release();
-
-	auto req = rovi_planner::moveit_planner::traj_moveit(
-														make_pose({0.50, 0.49, 0.1, 0, 0, 0}), 
-														"RRTConnect"
-														);
-
-	rovi_planner::moveit_planner::execution(
-											req, 
-											joint_state_pub
-											);
-
+	rovi_planner::moveit_planner::update_planning_scene();
+	rovi_planner::moveit_planner::start_planning_scene_publisher();
 	
+	// PICK
+	{
 
-	rovi_planner::moveit_planner::destruct();
+	// release gripper
+	ROS_INFO_STREAM("Releasing gripper...");
+	ur5_controllers::wsg::release();
+	ros::Duration(0.5).sleep();
 
+	// update moving planning scene from gazebo
+	ROS_INFO_STREAM("Updating planning scene...");
+	rovi_planner::moveit_planner::update_planning_scene();
+
+	// plan using moveit (to object)
+	ROS_INFO_STREAM("Planning...");
+	auto plan = rovi_planner::moveit_planner::plan(make_pose(POSE_BOTTLE_PICK), "RRTstar");
+
+	// get parabolic trajectory (time optimal)
+	ROS_INFO_STREAM("Creating trajectory (interpolation)...");
+	auto traj = rovi_planner::moveit_planner::get_traj(plan, 0.0001, 1/FREQ, 0.2, 0.1);
+
+	// execute in Gazebo
+	ROS_INFO_STREAM("Executing trajectory in Gazebo...");
+	ur5_controllers::ur5::execute_traj(traj, FREQ);
+
+	// update moving planning scene from gazebo
+	ROS_INFO_STREAM("Updating planning scene...");
+	rovi_planner::moveit_planner::update_planning_scene();
+
+	// grasp
+	ROS_INFO_STREAM("Grasping object...");
 	ur5_controllers::wsg::grasp();
+	ros::Duration(0.5).sleep();
 
-	ros::Duration(2).sleep();
+	// update moving planning scene from gazebo
+	ROS_INFO_STREAM("Updating planning scene...");
+	rovi_planner::moveit_planner::update_planning_scene();
 
-	req = rovi_planner::moveit_planner::traj_moveit(
-														make_pose({0.50, 0.49, 0.1, 0, 0, 0}), 
-														"RRTConnect"
-														);
+	// attach object (and implcit update)
+	ROS_INFO_STREAM("Attaching object to gripper...");
+	rovi_planner::moveit_planner::attach_object_to_ee("bottle");
+	
+	}
+	
+	// PLACE
+	{
+	
+	// plan using moveit (to to place)
+	ROS_INFO_STREAM("Planning...");
+	auto plan = rovi_planner::moveit_planner::plan(make_pose(POSE_BOTTLE_PLACE), "RRTstar");
 
-	rovi_planner::moveit_planner::execution(
-											req, 
-											joint_state_pub
-											);
+	// get parabolic trajectory (time optimal)
+	ROS_INFO_STREAM("Creating trajectory (interpolation)...");
+	auto traj = rovi_planner::moveit_planner::get_traj(plan, 0.0001, 1/FREQ, 0.2, 0.1);
+	
+	// execute in Gazebo
+	ROS_INFO_STREAM("Executing trajectory in Gazebo...");
+	ur5_controllers::ur5::execute_traj(traj, FREQ);
+	
+	// release gripper
+	// ROS_INFO_STREAM("Releasing gripper...");
+	// ur5_controllers::wsg::release();
+	// ros::Duration(0.5).sleep();
+	
+	// update moving planning scene from gazebo
+	ROS_INFO_STREAM("Updating planning scene...");
+	rovi_planner::moveit_planner::update_planning_scene();
+	
+	}
 
-	while(ros::ok());
+	// exit
+	std::cout << "\nPress any key to continue..." << std::endl;
+	std::cin.ignore();
+	rovi_planner::moveit_planner::destruct();
+	exit(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	//rovi_utils::export_traj(traj_joints, "traj_jnt_rrt_lin.csv");
-	
-
 
 	// trajectory display
 	// moveit_msgs::DisplayTrajectory traj_msg;
@@ -102,24 +156,10 @@ main(int argc, char** argv)
 	// // publish in rviz
 	// traj_pub.publish(traj_msg);
 
-	// create joint trajectory using linear interpolation
-	// auto joint_states = rovi_utils::joint_states_from_traj(*request.trajectory_);
-	// auto traj_joints  = rovi_planner::traj_linear(joint_states, 0.1, 0.1, 0.01);
-	// rovi_utils::export_traj(traj_joints, "traj_jnt_rrt_lin.csv");
-
-	// for (int i = 0; request.trajectory_->getWayPointCount() > i; i++)
-	// {
-	// 	ROS_INFO_STREAM( request.trajectory_->getWayPointDurationFromStart(i));
-	// }
-
 	// moveit::core::RobotStatePtr robot_state;
 
 
-	// terminate planner instance
-	// planner_instance->terminate();
-	
-
-	//traj_msg.
+	// traj_msg.
 
 	// set the robot state to the desided pose
 	// auto & q_last = res_msg.trajectory.joint_trajectory.points.back();
@@ -140,7 +180,7 @@ main(int argc, char** argv)
 	// 	while(ros::Time::now() - begin < timeout)
 	// 	{
 	// 		traj_pub.publish(traj_msg);
-	// 		planning_scene_pub.publish(planning_scene_msg);
+	// 		pub_planning_scene.publish(planning_scene_msg);
 	// 		lp.sleep();
 	// 	}
 	// };
