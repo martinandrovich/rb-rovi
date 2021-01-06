403bc2124c6436fce23365d20d010d7aa9bf2bbf
Martin Androvich <martinandrovich@gmail.com>
2021-01-06 17:37:32 +0100

    add common colors

--- ws/src/rovi_utils/matlab/rovi_common.m
+++ ws/src/rovi_utils/matlab/rovi_common.m
@@ -22,6 +22,13 @@ set(groot, "DefaultFigureRenderer", "painters");
 set(groot, "DefaultFigurePosition", [0 0 500 500]);
 set(groot, "DefaultFigureColor", [1 1 1]);
 
+% colors
+COL_GRAY = [200 200 200]/255;
+COL_LIGHTGRAY = [220 220 220]/255;
+COL_ORANGE = [255 143 0]/255;
+COL_BLUE = [0 207 255]/255;
+COL_MAGENTA = [236 88 234]/255;
+
 % log info
 disp("DIR_ROOT: " + what(DIR_ROOT).path)
 % display("DIR_DATA: " + what(DIR_DATA).path)
